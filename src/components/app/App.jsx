f8c8dc18f1e23d89c99935944a112a7318981692
Feldbush <feldbush@live.ru>
2021-02-10 21:43:19 +0300

    1.Сделал черновые наброски компонентов 2.Прописал роуты 3.Заменил ссылку на Link в шапке

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import MainPage from '../main/MainPage';
+import SignIn from '../signIn/SignIn';
+import Favorites from '../favorites/Favorites';
+import Offer from '../offer/Offer';
+import Page404 from '../page404/Page404';
 
 const App = (props) => {
   const {countCards} = props;
 
   return (
-    <MainPage countCards={countCards}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage countCards={countCards}/>
+        </Route>
+        <Route exact path='/login'>
+          <SignIn/>
+        </Route>
+        <Route exact path='/favorites'>
+          <Favorites/>
+        </Route>
+        <Route exact path='/offer/:id'>
+          <Offer/>
+        </Route>
+        <Route>
+          <Page404/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
